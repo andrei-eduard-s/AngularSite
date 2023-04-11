b33f52a1c45a83ab0d5b60ae1e6b06c30febef80
Andrei_Eduard_S <solomonandreieduard1@gmail.com>
2023-04-11 16:13:53 +0300

    LaboratorOnline

--- src/app/pages/inventory/inventory.component.ts
+++ src/app/pages/inventory/inventory.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ItemService } from 'src/app/services/item.service';
+import { Item } from 'src/models/item';
+import { FormComponent } from './form/form.component';
+// import { InventoryComponent } from 'src/app/pages/inventory';
 
 @Component({
   selector: 'app-inventory',
@@ -6,6 +11,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inventory.component.scss']
 })
 export class InventoryComponent implements OnInit {
+  error?: string;
+  itemList!: Item[];
   items: string[] = [
     'Andrei',
     'Marius',
@@ -14,7 +21,26 @@ export class InventoryComponent implements OnInit {
     'Andreea',
     'Mihaela',
   ];
-constructor() { }
+constructor(public dialog: MatDialog, public itemService: ItemService) { }
+getItems(): void {
+  this.itemService.getItems().subscribe((list: Item[]) => {
+    this.itemList = list;
+  }, (err: { error: string | undefined; }) => {
+    this.error = err.error;
+  })
+}
+
+async openDialog() {
+  const dialogRef = this.dialog.open(FormComponent, {
+    width: '250px',
+    data: {items: this.items},
+  });
+
+  dialogRef.afterClosed().subscribe(() => {
+    console.log('The dialog was closed');
+  });
+};
+
 ngOnInit(): void {
   
 }
